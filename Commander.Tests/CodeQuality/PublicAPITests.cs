6c6163e99d2bbc7d5bc976fcd72ba1357948f55c
Lasse V. Karlsen <lasse@vkarlsen.no>
2015-05-18 12:08:19 +0200

    Added more code quality tests.

--- Commander.Tests/CodeQuality/PublicAPITests.cs
+++ Commander.Tests/CodeQuality/PublicAPITests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
@@ -27,6 +28,25 @@ namespace Commander.Tests.CodeQuality
             bool attributeIsPresent = method.GetCustomAttributes(typeof(PublicAPIAttribute), true).Any();
             Assume.That(method.DeclaringType != null);
             Assert.That(attributeIsPresent, "Method " + method.Name + " of type " + method.DeclaringType.Name + " is not tagged with [PublicAPI]");
+        }
+
+        public IEnumerable<MethodInfo> AllPublicMethodsReturningReferenceTypes()
+        {
+            return from method in AllPublicMethods()
+                   where method.ReturnType != typeof(void)
+                   select method;
+        }
+
+        [Test]
+        [TestCaseSource(nameof(AllPublicMethodsReturningReferenceTypes))]
+        [Conditional("JETBRAINS_ANNOTATIONS")]
+        public void PublicMethod_ThatReturnsReferenceType_MustBeTaggedWithNotNullOrCanBeNullAttributes(MethodInfo method)
+        {
+            bool hasNotNullAttribute = method.GetCustomAttributes(typeof(NotNullAttribute), true).Any();
+            bool hasCanBeNullAttribute = method.GetCustomAttributes(typeof(CanBeNullAttribute), true).Any();
+
+            Assume.That(method.DeclaringType != null);
+            Assert.That(hasNotNullAttribute || hasCanBeNullAttribute, "Method " + method.Name + " of type " + method.DeclaringType.Name + " must be tagged with [NotNull] or [CanBeNull]");
         }
     }
 }
